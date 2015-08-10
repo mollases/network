6f1743a9e99deb163828d14b91f7e91590cd7d6a
Admas Molla <molla.admas@gmail.com>
2015-08-09 17:00:21 -0700

    working network

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,15 +1,39 @@
 'use strict';
 
-angular.module('network.networkCtrl', ['ngRoute'])
+angular.module('network.networkCtrl', ['ngRoute','ngVis'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'networkCtrl'
   });
 }])
 
-.controller('networkCtrl', [function() {
+.controller('networkCtrl', ['$scope',function($scope) {
+    var dataGroups = new vis.DataSet();
+    dataGroups.add({});
+    var dataNodes = new vis.DataSet();
+    dataNodes.add([
+        {id: 1, label: '1'},
+        {id: 2, label: '2'},
+        {id: 3, label: '3'},
+        {id: 4, label: '4'},
+        {id: 5, label: '5'}
+      ]);
+    var dataEdges = new vis.DataSet();
+    dataEdges.add([
+        {from: 1, to: 3},
+        {from: 3, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+    ]);
 
-
+    $scope.graphOptions = {
+        physics: {enabled: true}
+    };
+    $scope.graphData = {
+      nodes: dataNodes,
+      edges: dataEdges
+    };
 }]);
